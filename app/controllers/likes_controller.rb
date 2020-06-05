24e50faa796e97cd2e18088e123256751305205d
Andrew Birt <abirt1986@gmail.com>
2020-06-05 07:41:35 -0400

    dont duplicate notifications for liking the same post more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.create(user_id: current_user.id, post_id: params[:post_id])
-    Notification.create(receiver: @post.author, actor: current_user, action: "liked", notifiable: @post) unless @post.author == current_user
+    Notification.create(receiver: @post.author, actor: current_user, action: "liked", notifiable: @post) unless helpers.current_user_or_already_liked(@post)
     respond_to do |format|
       format.html { render partial: "users/shared/likes" }
       format.js 
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,5 @@
 module LikesHelper
+  def current_user_or_already_liked(post)
+    post.author == current_user || Notification.find_by(actor: current_user, action: "liked", notifiable: post)
+  end
 end
