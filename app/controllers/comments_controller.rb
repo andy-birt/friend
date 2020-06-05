f53be61baecf09656787ae5def266d9667a8a1d1
Andrew Birt <abirt1986@gmail.com>
2020-06-05 13:31:35 -0400

    users can comment on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,23 @@
 class CommentsController < ApplicationController
   def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
   end
 
   def new
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    @post.comments.create(user: current_user, body: params[:body])
+    Notification.create(receiver: @post.author, actor: current_user, action: "commented", notifiable: @post)
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 
   def edit
