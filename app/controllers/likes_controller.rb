61109336967d23ed416be28cde622b7ab287b018
Andrew Birt <abirt1986@gmail.com>
2020-06-04 21:39:31 -0400

    improving like system

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,20 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.create(user_id: current_user.id, post_id: params[:post_id])
+    Notification.create(receiver: @post.author, actor: current_user, action: "liked", notifiable: @post) unless @post.author == current_user
+    respond_to do |format|
+      format.html { render partial: "users/shared/likes" }
+      format.js 
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    Like.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { render partial: "users/shared/likes" }
+      format.js 
+    end
   end
 end
