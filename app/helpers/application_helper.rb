7e5d580f61f1b3bbbd64cf1fd6ce5d44862625d4
Andrew Birt <abirt1986@gmail.com>
2020-06-16 09:53:36 -0400

    add default avatar/add helper method to application to render default avatar when one isn't attached/configure views to use helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar(user, w, h)
+    if user.avatar.attached?
+      image_tag(user.avatar.variant(resize_to_fill: [w, h]), class: "rounded-circle")
+    else
+      image_tag("defaultavatar.png", width: w, height: h, class:"bg-secondary rounded-circle")
+    end
+  end
 end
