fea3dd3b76971de731b5eedef78413d24d60fad3
Andrew Birt <abirt1986@gmail.com>
2020-07-09 18:42:37 -0400

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  host = "sheltered-dusk-51864.herokuapp.com"
+  host = "peaceful-dawn-04960.herokuapp.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: host }
